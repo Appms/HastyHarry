588989c2c3f0e252d44bdf6ecea1bbea2f76fc7c
AMS <amserik@hotmail.com>
2016-03-10 22:53:57 +0100

    Turrets :bangbang:

--- game/src/mge/LightingDemo.cpp
+++ game/src/mge/LightingDemo.cpp
@@ -51,7 +51,17 @@ void LightingDemo::_initializeScene()
 {
     _renderer->setClearColor(0,0,0);
 
-	Level::Load("Level_01.xml", _world);
+	Level::Load("Goal.xml", _world);
+
+	Mesh* planeMeshDefault = Mesh::load(config::MGE_MODEL_PATH + "Billboard.obj");
+	AbstractMaterial* textureMaterial = new TextureMaterial(Texture::load(config::MGE_TEXTURE_PATH + "land.png"));
+	
+	GameObject* billboard = new GameObject("Billboard", glm::vec3(0, 0, 0), GameObject::PhysicsType::ANIMATEDBODY);
+	_world->add(billboard);
+	billboard->setMesh(planeMeshDefault);
+	billboard->scale(glm::vec3(1.0f, 0.5f, 1.0f));
+	billboard->setMaterial(textureMaterial);
+	billboard->setBehaviour(new Turret(Level::CurrentPlayer, 10.0f));
 
 	//Init Light
     Light* light = new Light("Light",glm::vec3(3.0f,3.0f,3.0f),glm::vec3(-2.0f,-1.0f,-3.0f),glm::vec3(1.0f,1.0f,1.0f),2.0f,60.0f,false);
